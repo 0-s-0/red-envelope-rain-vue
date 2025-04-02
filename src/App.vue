703a355683758b9d87e88180d04de74fa9a17709
0-s-0 <xushicai@proton.me>
2025-04-02 18:04:33 +0200

    初始化

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <redEnvelopeRainVue style="position: fixed;left: 0;top: 0;bottom: 0;right: 0;" :num="100"></redEnvelopeRainVue>
+  </div>
+</template>
+
+<script setup lang="ts">
+import redEnvelopeRainVue from "red-envelope-rain-vue"
+</script>
+
+<style scoped lang="less"></style>
--- src/utils/initializationOfNumericalUnits.ts
+++
@@ -1,7 +0,0 @@
-export default (value: number | string) => {
-  if (Number(value).toString() === "NaN") {
-    return value;
-  } else {
-    return value + "px";
-  }
-};
