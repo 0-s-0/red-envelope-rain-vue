18ed812c1991cab69b2351a72fd5cd3779378f70
0-s-0 <xushicai@proton.me>
2025-04-02 18:30:55 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <redEnvelopeRainVue style="position: fixed;left: 0;top: 0;bottom: 0;right: 0;" :num="100"></redEnvelopeRainVue>
+    <redEnvelopeRainVue style="position: fixed;left: 0;top: 0;bottom: 0;right: 0;" :num="100" :icon="redEnvelopeRainCard"></redEnvelopeRainVue>
   </div>
 </template>
 
 <script setup lang="ts">
 import redEnvelopeRainVue from "red-envelope-rain-vue"
+import redEnvelopeRainCard from './assets/redEnvelopeRainCard.png'
 </script>
 
 <style scoped lang="less"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import "red-envelope-rain-vue/dist/style.css"
 
 createApp(App).mount('#app')
