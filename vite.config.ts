006960eaf8ef36e77a2ebec03ea943be44f9b97c
0-s-0 <xushicai@proton.me>
2025-04-02 18:52:11 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' 
+  ? '/red-envelope-rain-vue/dist' // 这里是 GitHub 仓库的名称
+  : '/', // 本地开发时使用根路径
 })
